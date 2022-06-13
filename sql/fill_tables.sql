f46be3146aa9e685a316f54aad3a35fe0d795abf
Arturo Cuicas <orutrax@gmail.com>
2022-06-12 20:37:59 -0400

    Fill tables

--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -1,8 +1,43 @@
--- -- Create User
--- DECLARE userUUID uuid := uuid_generate_v4 ();
+-- User
+WITH c (
+    user_uuid
+    ) as (
+   values (
+        uuid_generate_v4 ()
+   )
+)
 
--- INSERT INTO "user" (uuid, full_name) VALUES 
--- (UserUUID, 'Jhon Doe');
+insert into "user" (uuid, full_name) values(
+    (select user_uuid from c),
+    'John Doe'
+);
 
--- INSERT INTO "name" (user_uuid, name) VALUES 
--- (UserUUID, 'first_budget');
+-- Budget
+insert into "budget" (user_uuid, name) values (
+    (select uuid from "user" limit 1),
+    'first_budget'
+);
+
+-- Categories
+with c (
+    budget_uuid
+    ) as (
+    values (
+        (select uuid as budget_uuid from "budget" limit 1)
+    )
+)
+
+insert into "category" (budget_uuid, name) values
+    ((select budget_uuid from c limit 1), 'Food'),
+    ((select budget_uuid from c limit 1), 'Transportation'),
+    ((select budget_uuid from c limit 1), 'Housing'),
+    ((select budget_uuid from c limit 1), 'Savings & Investments');
+
+-- Entries
+insert into "entry" (user_uuid, category_uuid, description, amount)
+select
+    (select uuid from "user" limit 1),
+    uuid,
+    name,
+    random () * (100-1) + 3
+from "category";
